4499a0bead79949d0cd3cf42b875272037b273d5
BrevnEl <lenadmitrieva2014@yandex.ru>
2023-08-19 20:12:33 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int i=20;
+        int b=10000;
 
-        System.out.println("Hello and welcome!");
+
+        System.out.println(b/20*1);
 
 
     }
