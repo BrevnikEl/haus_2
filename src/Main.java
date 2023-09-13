43dadbcd9a551041fda40fbeea287480999dae7e
BrevnEl <lenadmitrieva2014@yandex.ru>
2023-09-13 16:06:38 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int cost = 10_000;
         int bonus = 20;
-        int mill = cost/ bonus;
-        System.out.println(mill);
+        int mill = cost / bonus;
+        System.out.println("Вам начислено: " + mill + " бонусных милей!");
 
 
     }
